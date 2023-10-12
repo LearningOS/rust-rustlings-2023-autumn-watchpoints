d5dc507830d7fd56313c600b104e1900b64571ec
程序员小王(troy) troy <wang_cyi@163.com>
2023-10-12 07:51:36 +0000

    string

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -1,17 +1,18 @@
 // strings1.rs
 //
 // Make me compile without changing the function signature!
+// 我在不更改函数签名的情况下编译！
 //
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
 }
-
+// String type, which is provided by Rust’s standard library rather than coded into the core language, is a growable, mutable, owned, UTF-8 encoded string type.
 fn current_favorite_color() -> String {
-    "blue"
+    "blue".to_string()
 }
