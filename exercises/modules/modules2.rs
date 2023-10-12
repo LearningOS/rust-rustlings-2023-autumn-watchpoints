94e4411c7d0a8eaa35ba397ac5c573112f4eabb9
程序员小王(troy) troy <wang_cyi@163.com>
2023-10-12 11:57:50 +0000

    modules3

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -3,16 +3,20 @@
 // You can bring module paths into scopes and provide new names for them with
 // the 'use' and 'as' keywords. Fix these 'use' statements to make the code
 // compile.
-//
+//您可以将模块路径引入范围并为它们提供新名称
+//“use”和“as”关键字。 修复这些“use”语句以生成代码
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+//https://doc.rust-lang.org/book/ch07-02-defining-modules-to-control-scope-and-privacy.html
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    //use self::fruits::PEAR as ???
+    //use self::veggies::CUCUMBER as ???
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
+
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -4,15 +4,19 @@
 // anywhere and especially from the Rust standard library into your scope. Bring
 // SystemTime and UNIX_EPOCH from the std::time module. Bonus style points if
 // you can do it with one line!
+
+//您可以使用 'use' 关键字将来自任何地方的模块的模块路径，\\\
+// 尤其是来自 Rust 标准库的模块路径引入您的范围。从 std：：time 模块中引入 SystemTime 和 UNIX_EPOCH。奖励风格点，如果你能用一行做到！
 //
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // TODO: Complete this use statement
-use ???
-
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+//https://rustwiki.org/zh-CN/book/ch07-04-bringing-paths-into-scope-with-the-use-keyword.html
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
