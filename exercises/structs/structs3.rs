2c8e7f7570f0800abae6d7bd9b2c345d02660373
程序员小王(troy) troy <wang_cyi@163.com>
2023-10-12 05:43:08 +0000

    enum

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -3,11 +3,10 @@
 // Structs contain data, but can also have logic. In this exercise we have
 // defined the Package struct and we want to test some logic attached to it.
 // Make the code compile and the tests pass!
-//
+// 结构包含数据，但也可以具有逻辑。在本练习中，我们定义了 Package 结构，并希望测试附加到它的一些逻辑。使代码编译并通过测试！
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -15,8 +14,10 @@ struct Package {
     recipient_country: String,
     weight_in_grams: i32,
 }
-
+//https://doc.rust-lang.org/std/keyword.impl.html
+//The impl keyword is primarily used to define implementations on types.
 impl Package {
+    //c++ 中的静态方法 不需要实例化
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
             panic!("Can not ship a weightless package.")
@@ -29,12 +30,16 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        self.sender_country != self.recipient_country
+        //https://doc.rust-lang.org/book/ch05-03-method-syntax.html
     }
-
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    
+    fn get_fees(&self, cents_per_gram: i32) -> i64 {
         // Something goes here...
+        //计算套餐的费用
+        i64::from(self.weight_in_grams) * i64::from(cents_per_gram)
     }
 }
 
