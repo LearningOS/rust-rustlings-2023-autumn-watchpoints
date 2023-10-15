f4552b115a68887fe7307fbd4e62706a8a720bcc
程序员小王(troy) troy <wang_cyi@163.com>
2023-10-15 09:42:39 +0000

    MAC

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,8 +5,8 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
@@ -18,3 +18,7 @@ mod macros {
 fn main() {
     my_macro!();
 }
+
+
+// 通过在代码中使用 #[macro_use] 属性，您可以全局地导入宏定义，而不仅仅是在一个特定的函数或模块中。
+// 这有助于确保宏的定义在整个项目中都可用，而不会出现未使用宏的警告。
